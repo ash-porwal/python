74eb66f94fe43400b9f5a383f8ee49ca9d35f0a9
“ash-porwal” <ashporwal11@gmail.com>
2024-05-22 19:11:28 +0530

    updated file

--- 0python.py
+++ 0python.py
@@ -5,11 +5,26 @@ Python was released by Guido van Rossum in 1991
 -----------------------------------------------
 - Python is Interpreted Language -
   That means that, unlike languages like C and its variants, Python does not need to be compiled before it is run. Other interpreted languages include PHP and Ruby.
+  Programs written in interpreted languages are executed by an interpreter, which reads the source code line-by-line or block-by-block, translates it into machine code, and runs it immediately. This process happens every time the program is run.
 
-- Python is Dynamically typed Language - means the type of our variables are going to be checked at run time and not at the compiled time.
+  Compiled Languages: Programs written in compiled languages are transformed (compiled) into machine code by a compiler before they are run. The output is a standalone executable file that can be run on its own without further translation. This compilation is done only once, and the executable can be run many times afterward.
+
+- Python is Dynamically typed Language - 
+    Python is described as a dynamically typed language because the type of a variable is determined at runtime, not in advance.
     dynamically typed, this means that you don't need to state the types of variables when you declare them or anything like that. 
 
+    Advantages of Dynamic Typing:
+      1. Rapid Development: Faster prototyping and fewer lines of code to write since there is no need to define types.
+      2. Ease of Learning: Simplifies the learning curve for new programmers because they can focus on programming concepts without worrying about static type definitions.
+      3. High Flexibility: Supports polymorphism seamlessly, allowing the same function to process objects of different classes.
+    Disadvantages of Dynamic Typing:
+      1. Performance: Generally, dynamically typed languages are slower than statically typed languages because type checking is done at runtime.
+      2. Potential for Runtime Errors: Errors related to type mismatch might only occur at runtime, which can lead to bugs that are detected later in the development cycle or even after deployment.
+      3. Lack of Immediate Clarity: In larger codebases or with multiple developers, it might be less clear what type of data a function expects or returns, leading to misuse or bugs.
+
 - In Python, functions are first-class objects. 
+ "first-class object" refers to any entity that can be dynamically created, destroyed, passed to a  
+  function, returned as a value, and assigned to a variable.
   This means that they can be assigned to variables, returned from other functions and passed into functions. Classes are also first class objects
 
 - Python is a high level language - When we say "Python is a high-level language," 
@@ -80,12 +95,65 @@ Python was released by Guido van Rossum in 1991
 
 
 -----------------------------------------------
+- Different types of memory in Python?
+  Types of memory - 
+  1. Stack memory - all methods, function calls, references are stored in this.
+  2. Private heap memory - all values and objects are stored
+
+  for example
+  x = 5
+  so, here x is referring to an object so it will get saved in Stack memory
+  while 5 is value, so will get saved in Private heap 
+
+  >>> x = 5
+  >>> id(x)
+  4309451120
+  >>> x = x+1 # here now x is refering to the new memory location
+  >>> id(x)
+  4309451152
+  >>> 
+  >>> y = 5 # as 5 was already in Private heap, so y is now pointing to the same memory location.
+  >>> id(y)
+  4309451120 # see? same id as x
+  >>> 
+  So, if we get something in Heap memory, so one or more than one object can refer that heap memory.
+
+     
+  When, there a value in Private Heap memory and no object refers to that memory, then
+  that value in heap memory is considered as Dead object - and this mechanism is called reference counting. And that dead value will move to grabage collector
+
 - How Memory is managed in Python?
-  There is Private heap which stores all object and data strucuture in python,
-  As a developer we don't have access to this private heap and it is taken care by Python Interpreter
-  Python's memory manager allocate heap space.
-  Python has an inbuilt garbage collector which recycles all unused memory and can be made available
-  to heap space.
+  Python manages its memory through a private area called the "heap" where all Python objects and values are stored. This area is only accessible by the Python interpreter, which means regular programs can't access it directly.
+
+  Here's how Python handles memory:
+  1. Memory Allocation: Python has a memory manager that takes care of setting aside memory in the heap for Python objects. This system makes sure Python programs have the memory they need to run.
+
+  2. Garbage Collection: Python uses a built-in system called a "garbage collector" to reclaim memory once objects are no longer needed. When a Python object is no longer used by the program (meaning nothing in the program is referring to it anymore), the garbage collector removes it from memory. This process helps Python make efficient use of memory by freeing up space that's no longer being used.
+
+- What do you mean by Grabage collector and reference counting?
+  Reference counting:
+    Reference counting is memory management technique, in which the object gets dellocated when there is no reference to that in program.
+    there reference are always counted and stored in the memory.
+    When reference count of an object reaches 0 then reference counting garbage collection algorithm
+    cleans up the object immediately.
+    for example - x=5 # in this x is pointing to 5, so it has one reference 
+    now, if we delocate and point x to a new value then nothing is pointing to 5 thus reference count becomes 0, so now reference counting garbage collection algorithm will get trigger and cleans up the memory.
+  
+  Garbage collector:
+    Python deleted unwanted objects automatically to free the memory space.
+    The process by which Python cleans up the memory and reclaim the memory is called Garbage Collector.
+    Python garbage collector runs during the program execution and is triggerd when object is 
+    reference count reaches 0.
+    The gc module defines the function to enable or disable garbage collector
+    - gc.enable()
+    - gc.disable()
+
+
+- What is Duck typing and why python is called dynamically types language?
+  Python don't have problem with if we don't declare the type of the variable.
+  It gets to type of variable in the runtime.
+  Python also takes care of the memory management that's why Python is a dynamically typed language.
+
 -----------------------------------------------
 
 
@@ -94,18 +162,41 @@ Python was released by Guido van Rossum in 1991
   Module - it is a code file which contains collection of functions, classes and variables (example - random module, JSON, OS)
   Packages - it is a collection of module, and this package dir will also have __init__.py so that we can import modules.
             and in general we import module from packages 
-  Library - it is a collection of python library(example - requests, Pandas, Numpy)
+  Library - it is a collection of python packages(example - requests, Pandas, Numpy)
 -----------------------------------------------
 
 -----------------------------------------------
 - Difference between range & xrange
+  xrange was in python2
+  range is in pyhton3
   The only difference is that range returns a Python list object 
-  and x range returns an xrange object. It creates the values as you need them with a special technique called yielding.
+  and xrange returns generator object. It creates the values as you need them with a special technique called yielding. That's is why xrange is called lazy evaluation.
+
+  For example:
+    a = range(1, 5000)
+    b = xrange(1, 5000)
+
+    print("The return type of range() is : ")
+    print(type(a)) # list object
+
+    print("The return type of xrange() is : ")
+    print(type(b)) # xrange object
+  
+  Memory consumtion of range is higher as compare to xrange
+  we can proof this with
+  print(sys.getsizeof(a))
+  print(sys.getsizeof(b))
+
+  1. xrange of Python2 is now range in Python3
+  2. as range returns list object, so it is better than xrange when it comes to operation.
+  3. xrange is faster than range
+
+
 -----------------------------------------------
 
 -----------------------------------------------
 - What is pickling and unpickling?
-  Pickling is the process of converting a Python object into a byte stream. 
+  Pickling is the process of converting a Python object into a byte stream. so that it can be saved in file.
   This serialized form of the object can be stored in a file or sent over a network. 
 
   import pickle
@@ -137,6 +228,7 @@ Python was released by Guido van Rossum in 1991
 - What is GIL?
   Global Interpreter Lock
   The GIL ensures that only one thread can execute Python bytecode at a time.
+  It is like a hurdle as it doesn't allow multi-threading in python.
 
   Although the GIL allows multiple threads to be created in a Python program, 
   it limits concurrent execution of Python bytecode, 
@@ -159,7 +251,7 @@ Python was released by Guido van Rossum in 1991
 -----------------------------------------------
 - Difference between append and extend?
   append - adds element at the end
-  extend - adds element from an iterable to the end of the list
+  extend - adds all elements from an iterable to the end of the list.
 -----------------------------------------------
 
 
